55f0ec651f101e9c19140f85f14d5ac3b8772e13
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-07-23 14:09:47 +0200

    complete file - initial commit

---
+++ spec/gossip_project_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/app/NOM_FICHIER_MAIN_CLASS_1_RB'
+require_relative '../lib/app/ETC_RB'
+
+describe 'la fonction METHOD' do
+
+  it 'DECRIRE METHOD' do
+    expect('METHOD'('ARG')).to eq('RESULTAT')
+  end
+end
+
